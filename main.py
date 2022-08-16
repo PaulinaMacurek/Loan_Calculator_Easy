04689a6df084d97726ec2e43a28d4184a5cc48dd
Paulina <paulinamacurek1@gmail.com>
2022-08-16 17:43:31 +0200

    Final version

--- main.py
+++ main.py
@@ -1,77 +1,68 @@
 import math
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description="This program calculate some loan parametrs.")
 
-parser.add_argument("-i1", "--type", choices=["annuity", "diff"], required = True,
+parser.add_argument("--type", choices=["annuity", "diff"],
                     help="You need to choose only one option from the list.")
-parser.add_argument("-i2", "--payment",
+parser.add_argument("--payment", type=int,
                     help="'--payment' is the monthly payment amount. Valid only with '--type = annuity'.")
-parser.add_argument("-i3", "--principal",
+parser.add_argument("--principal", type=int,
                     help="Enter the loan principal.")
-parser.add_argument("-i4", "--interest",
+parser.add_argument("--interest", type=float,
                     help="Enter interest without a percent sign.")
+parser.add_argument("--periods", type=int,
+                    help="Enter how many months do you want to repay your loan?.")
+args = parser.parse_args()  # The parse_args() method is used for reading argument strings from the command line
 
-args = parser.parse_args() # The parse_args() method is used for reading argument strings from the command line
-print(f"Type: {args.type}")
-if args.payment:
-    print(f"Payment: {args.payment}")
-else:
-    print("Nie podałeś payment")
+
+type_of_calc = args.type
+loan_principal = args.principal
+monthly_payment = args.payment
+nr_periods = args.periods
+loan_interest = args.interest
+
+def check_type(type):
+    if type is not None:
+        return type if type in ["diff", "annuity"] else False
+
 
 def months_to_years(nr_months_to_calculate):
-    nr_years = math.floor(nr_months_to_calculate / 12)
-    nr_months = nr_months_to_calculate - nr_years * 12
-    return [nr_years, nr_months]
+    nr_years, nr_months = divmod(nr_months_to_calculate, 12)
+    if nr_years >= 1 and nr_months >= 1:
+        print(f"It will take {nr_years}", "years" if nr_years != 1 else "year",
+        f"and {nr_months} months" if nr_months != 1 else "month", "to repay this loan!")
+    elif nr_years == 0 and nr_months >= 1:
+        print(f"It will take {nr_months}", "months" if nr_months != 1 else "month", "to repay this loan!")
+    else:
+        print(f"It will take {nr_years}", "years" if nr_years != 1 else "year", "to repay this loan!")
+    overpayment = nr_months_to_calculate * monthly_payment - loan_principal
+    if overpayment:
+        print(f"Overpayment = {overpayment}")
 
 
 def nbr_monthly_payments():
-    print(msgs["msg_1"])
-    loan_principal = int(input())
-    print(msgs["msg_3"])
-    monthly_payment = int(input())
-    print(msgs["msg_5"])
-    loan_interest = float(input()) / 1200  # divide by 12 and 100
-    result = math.ceil(
-        math.log(monthly_payment / (monthly_payment - loan_interest * loan_principal), 1 + loan_interest))
+    result = math.ceil(math.log(monthly_payment / (monthly_payment - loan_interest * loan_principal), 1 + loan_interest))
     return result
 
 
 def annuity_monthly_payment_amount():
-    print(msgs["msg_1"])
-    loan_principal = int(input())
-    print(msgs["msg_6"])
-    nr_periods = int(input())
-    print(msgs["msg_5"])
-    loan_interest = float(input()) / 1200  # divide by 12 and 100
-    result = math.ceil(
-        loan_principal * (loan_interest * (1 + loan_interest) ** nr_periods) / ((1 + loan_interest) ** nr_periods - 1))
+    result = math.ceil(loan_principal * (loan_interest * (1 + loan_interest) ** nr_periods) / ((1 + loan_interest) ** nr_periods - 1))
     print(f"Your monthly payment = {result}!")
     overpayment = result * nr_periods - loan_principal
     if overpayment:
         print(f"Overpayment = {overpayment}")
 
 
-def loan_principal():
-    print(msgs["msg_7"])
-    annuity_payment = float(input())
-    print(msgs["msg_6"])
-    nr_periods = int(input())
-    print(msgs["msg_5"])
-    loan_interest = float(input()) / 1200  # divide by 12 and 100
+def loan_principal_fun():
     result = round(
-        annuity_payment * ((1 + loan_interest) ** nr_periods - 1) / (loan_interest * (1 + loan_interest) ** nr_periods))
+        monthly_payment * ((1 + loan_interest) ** nr_periods - 1) / (loan_interest * (1 + loan_interest) ** nr_periods))
     print(f"Your loan principal = {result}!")
     return result
 
 
 def differentiated_payments():
-    print(msgs["msg_1"])
-    loan_principal = int(input())
-    print(msgs["msg_6"])
-    nr_periods = int(input())
-    print(msgs["msg_5"])
-    loan_interest = float(input()) / 1200  # divide by 12 and 100
     suma = 0
     for i in range(1, nr_periods + 1):
         result = loan_principal / nr_periods + loan_interest * (loan_principal - (i - 1) * loan_principal / nr_periods)
@@ -83,46 +74,32 @@ def differentiated_payments():
         print(f"\nOverpayment = {overpayment}")
 
 
-# wieczorem zrobiłam diggerentiated_payments() oraz annuity_monthly_payment_amount():
+parametrs = [monthly_payment, loan_principal, loan_interest, nr_periods]
+for item in parametrs:
+    if item == None:
+        continue
+    elif item < 0:
+        print("Incorrect parameters")
+        sys.exit(0)
 
-msgs = {"msg_1": "Enter the loan principal:",
-        "msg_2": "What do you want to calculate? \ntype 'n' for number of monthly payments,\ntype 'a' for annuity monthly payment amount,\ntype 'p' for loan principal",
-        "msg_3": "Enter the monthly payment:",
-        "msg_4": "Enter the number of months:",
-        "msg_5": "Enter the loan interest:",
-        "msg_6": "Enter the number of periods:",
-        "msg_7": "Enter the annuity payment:"}
+if loan_interest != None:
+    loan_interest=loan_interest/1200
 
-print(msgs["msg_2"])
-choice = input()
-if choice == "n":
-    result = months_to_years(nbr_monthly_payments())
-    if result[0] != 1:
-        if result[1] == 0:
-            print(f"It will take {result[0]} years to repay this loan!")
-        elif result[1] == 1:
-            print(f"It will take {result[0]} years and 1 month to repay this loan!")
-        else:
-            print(print(f"It will take {result[0]} years and {result[1]} months to repay this loan!"))
-    elif result[0] == 1:
-        if result[1] == 0:
-            print(f"It will take 1 year to repay this loan!")
-        elif result[1] == 1:
-            print(f"It will take 1 year and 1 month to repay this loan!")
-        else:
-            print(print(f"It will take 1 year and {result[1]} months to repay this loan!"))
-    elif result[0] == 0:
-        if result[1] == 0:
-            print(f"Somthing went wrong... you dont have to repay your loan")
-        elif result[1] == 1:
-            print(f"It will take 1 month to repay this loan!")
-        else:
-            print(print(f"It will take {result[1]} months to repay this loan!"))
-
-elif choice == "a":
-    annuity_monthly_payment_amount()
-elif choice == "p":
-    loan_principal()
+if check_type(type_of_calc) == 'diff':
+    if not monthly_payment and loan_principal and nr_periods and loan_interest:
+        differentiated_payments()
+    else:
+        print("Incorrect parameters")
+        sys.exit(0)
+elif check_type(type_of_calc) == "annuity":
+    if monthly_payment and not loan_principal and nr_periods and loan_interest:
+        loan_principal_fun()
+    elif monthly_payment and loan_principal and not nr_periods and loan_interest:
+        months_to_years(nbr_monthly_payments())
+    elif not monthly_payment and loan_principal and nr_periods and loan_interest:
+        annuity_monthly_payment_amount()
+    else:
+        print("Incorrect parameters")
+        sys.exit(0)
 else:
-  pass#  differentiated_payments()
-
+    print("Incorrect parameters")
